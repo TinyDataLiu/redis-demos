7d4c701a7757c5816aa15dab834bf15bef6a6640
卖小姑娘的火柴 <1360737271@qq.com>
2020-04-04 14:07:32 +0800

    spring session redis

--- spring-boot-redis-demo/src/main/java/com/alice/springboot/jedis/ShardingTest.java
+++ spring-boot-redis-demo/src/main/java/com/alice/springboot/jedis/ShardingTest.java
@@ -5,9 +5,11 @@ import redis.clients.jedis.JedisPoolConfig;
 import redis.clients.jedis.JedisShardInfo;
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPool;
+import redis.clients.util.Hashing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 public class ShardingTest {
@@ -17,11 +19,14 @@ public class ShardingTest {
         shardInfos.add(new JedisShardInfo("192.168.1.153", 6379));
         shardInfos.add(new JedisShardInfo("192.168.1.153", 7379));
 
-        ShardedJedisPool pool = new ShardedJedisPool(poolConfig, shardInfos);
+        ShardedJedisPool pool = new ShardedJedisPool(poolConfig, shardInfos, Hashing.MURMUR_HASH);
+
         ShardedJedis jedis = pool.getResource();
         for (int i = 0; i < 100; i++) {
-            log.info("ShardedJedisPool:{}={}", i, jedis.get("ShardedJedisPool:" + i));
+
 //            jedis.set("ShardedJedisPool:" + i, UUID.randomUUID().toString());
+
+            jedis.set("jedis:" + i, UUID.randomUUID().toString());
         }
         String s = jedis.get("ShardedJedisPool:1");
         System.out.println(s);
