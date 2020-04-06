5ede6fdb75ad43887850b0cdd1f8cf0807e75642
卖小姑娘的火柴 <1360737271@qq.com>
2020-04-06 14:21:26 +0800

    spring session jedis 客户端发布订阅

--- spring-boot-redis-demo/src/main/java/com/alice/springboot/jedis/lua/LuaTest.java
+++ spring-boot-redis-demo/src/main/java/com/alice/springboot/jedis/lua/LuaTest.java
@@ -5,17 +5,40 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 @Slf4j
 public class LuaTest {
 
     public static void main(String[] args) {
+        for (int i = 0; i < 12; i++) {
+            limitIp();
+        }
+    }
+
+
+    public static void simple() {
         final String KEY = "TEST:LUA:KEY";
         Jedis jedis = getJedis();
         jedis.eval("return redis.call('set',KEYS[1],ARGV[1])", 1, KEY, UUID.randomUUID().toString());
         log.info("{}", jedis.get(KEY));
+    }
 
+    public static void limitIp() {
+        Jedis jedis = getJedis();
+        // 只在第一次对key设置过期时间
+        String lua = "local num = redis.call('incr', KEYS[1])\n" +
+                "if tonumber(num) == 1 then\n" +
+                "\tredis.call('expire', KEYS[1], ARGV[1])\n" +
+                "\treturn 1\n" +
+                "elseif tonumber(num) > tonumber(ARGV[2]) then\n" +
+                "\treturn 0\n" +
+                "else \n" +
+                "\treturn 1\n" +
+                "end\n";
+        Object result = jedis.evalsha(jedis.scriptLoad(lua), Arrays.asList("localhost"), Arrays.asList("10", "10"));
+        log.info("result={}", result);
     }
 
 
